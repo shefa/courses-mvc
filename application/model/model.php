26b90dbe384f60c9f0c568aa67381cbc534e00e1
gesh <venom1724@gmail.com>
2018-04-26 19:52:30 +0300

    Update model.php

--- application/model/model.php
+++ application/model/model.php
@@ -3,8 +3,14 @@ class Model
 {
     function __construct($db)
     {
-        try $this->db = $db;
-        catch (PDOException $e) exit('Database connection could not be established.');
+        try
+        {
+            $this->db = $db;
+        }
+        catch (PDOException $e)
+        {
+            exit('Database connection could not be established.');
+        }
     }
 
     public function getAllCourses()
